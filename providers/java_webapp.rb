fc6c275d4f77c49a981c8fb37ddd5995ae2f9596
Yoni Yalovitsky <yoni@fewbytes.com>
2013-04-28 13:00:04 +0300

    Also truncated GET parameters in context template
    This follows a similar change in the remote file provider

--- providers/java_webapp.rb
+++ providers/java_webapp.rb
@@ -69,7 +69,8 @@ def create_context_file
       :app => new_resource.application.name,
       :host => host,
       :database => new_resource.database,
-      :war => "#{new_resource.path}/current/#{new_resource.war || ::File.basename(new_resource.application.repository)}"
+      :war => "#{new_resource.path}/current/#{new_resource.war ||
+                      ::File.basename(new_resource.application.repository).split("?").first}"
     )
   end
 end
